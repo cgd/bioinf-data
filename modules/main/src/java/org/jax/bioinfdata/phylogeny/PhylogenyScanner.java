24e0d0fc299dfb508653e4a6d028016ae8c414d0
Keith Sheppard <keithshep@gmail.com>
2011-06-17 15:14:47 -0400

    forgot to add "break;" (should improve efficiency)

--- modules/main/src/java/org/jax/bioinfdata/phylogeny/PhylogenyScanner.java
+++ modules/main/src/java/org/jax/bioinfdata/phylogeny/PhylogenyScanner.java
@@ -74,6 +74,7 @@ public class PhylogenyScanner
                        b != AbstractGenotypeCallMatrix.B_CALL_CODE)
                     {
                         allHomo = false;
+                        break;
                     }
                 }
                 
